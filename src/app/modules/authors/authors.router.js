88d7befd38e23a72bc4e04c31027af47a2ebef5a
Gerald Haxhi <gerald.haxhi@softup.co>
2020-10-27 22:23:13 +0100

    Small fixes

--- src/app/modules/authors/authors.router.js
+++ src/app/modules/authors/authors.router.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import passport from 'passport';
+import Passport from 'passport';
 
 import * as controller from './authors.controller';
 import * as validator from './authors.validator';
@@ -9,32 +9,32 @@ const router = new Router();
 const BASE_ROUTE = `/authors`;
 
 router.route(BASE_ROUTE).post(
-  passport.authenticate('jwt', { session: false }),
+  Passport.authenticate('jwt', { session: false }),
   authorization.updateAuthorAuthorization,
   validator.createAuthorValidator,
   controller.createAuthor,
 );
 
 router.route(BASE_ROUTE).get(
-  passport.authenticate('jwt', { session: false }),
+  Passport.authenticate('jwt', { session: false }),
   validator.readAuthorsValidator,
   controller.readAuthors
 );
 
 router.route(`${BASE_ROUTE}/:id`).get(
-  passport.authenticate('jwt', { session: false }),
+  Passport.authenticate('jwt', { session: false }),
   controller.readOneAuthor
 );
 
 router.route(`${BASE_ROUTE}/:id`).patch(
-  passport.authenticate('jwt', { session: false }),
+  Passport.authenticate('jwt', { session: false }),
   authorization.updateAuthorAuthorization,
   validator.updateAuthorValidator,
   controller.updateAuthor
 );
 
 router.route(`${BASE_ROUTE}/:id`).delete(
-  passport.authenticate('jwt', { session: false }),
+  Passport.authenticate('jwt', { session: false }),
   authorization.updateAuthorAuthorization,
   controller.deleteAuthor
 );
