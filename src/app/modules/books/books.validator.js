db83c2b2116f2129c8b36c1aa92b3b287fea36c8
Gerald Haxhi <gerald.haxhi@softup.co>
2020-10-29 22:59:53 +0100

    Small lint fix

--- src/app/modules/books/books.validator.js
+++ src/app/modules/books/books.validator.js
@@ -2,7 +2,7 @@ import BaseJoi from 'joi';
 import DateJoi from '@hapi/joi-date';
 
 import { BadRequest } from '../../utils/error';
-import { commaSeparatedWords, allowedImageTypes } from '../../helpers/validation';
+import { commaSeparatedWords } from '../../helpers/validation';
 import datetimeFormats from '../../constants/datetime_formats';
 
 const Joi = BaseJoi.extend(DateJoi);
